0c75a09f7ee3f56414094ed8368e1b0c8380aab3
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-29 16:51:28 +0300

    Добавил Exercise_7_5_03

---
+++ Module_7/Exercise_7_5_03/Models/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_5_03.Models
+{
+    internal class Helper
+    {
+        public static int temp;
+        public static void Swap(ref int num1,ref int num2)
+        {
+            temp = num1;
+            num1 = num2;
+            num2=temp;
+        }
+    }
+}
--- Module_7/Exercise_7_5_03/Program.cs
+++ Module_7/Exercise_7_5_03/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Exercise_7_5_03
+﻿using Exercise_7_5_03.Models;
+
+namespace Exercise_7_5_03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int num1 = 3;
+            int num2 = 15;
+            Helper.Swap(ref num1,ref num2);
         }
     }
 }
