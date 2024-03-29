5e96e35e9686d61ad792ed46f41cc8cc51609183
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-29 13:18:39 +0300

    Добавил Exercise_7_2_4

---
+++ Module_7/Exercise_7_2_4/Models/DerivedClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_2_4.Models
+{
+    internal class DerivedClass:BaseClass
+    {
+        public int counter;
+        public override int Counter 
+        { 
+            get
+            {
+                return counter;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    Console.WriteLine("Значение не должно быть меньше 0");
+                }
+                else
+                {
+                    counter = value;
+                }
+            }
+        }
+    }
+}
---
+++ Module_7/Exercise_7_2_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_7_2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
