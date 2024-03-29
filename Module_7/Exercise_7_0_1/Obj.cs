b93cb49acb899c5dd7ef12a500b65487d6ae9a76
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-29 11:44:40 +0300

    Добавлен Exercise_7_0_1

---
+++ Module_7/Exercise_7_0_1/Obj.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_0_1
+{
+    internal class Obj
+    {
+        public string name;
+        public string description;
+
+        public Obj()
+        {
+            Console.WriteLine("Вызван конструктор без параметров");
+        }
+
+        public Obj(string name,string description):this()
+        {
+            this.name = name;   
+            this.description = description;
+        }
+    }
+}
---
+++ Module_7/Exercise_7_0_1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise_7_0_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj=new Obj("Объект","Нет описания");
+            Console.WriteLine(obj.name+" "+obj.description);
+        }
+    }
+}
