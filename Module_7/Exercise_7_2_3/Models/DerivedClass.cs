e275a67066dcf7004dbe9c69472a11f3b33bb853
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-29 12:58:07 +0300

    Добавил Exercise_7_2_3

---
+++ Module_7/Exercise_7_2_3/Models/DerivedClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_2_3.Models
+{
+    internal class DerivedClass:BaseClass
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Метод класса DerivedClass");
+        }
+    }
+}
---
+++ Module_7/Exercise_7_2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exercise_7_2_3.Models;
+
+namespace Exercise_7_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseClass=new BaseClass();
+            var derivedClass=new DerivedClass();
+
+            baseClass.Display();
+            derivedClass.Display();
+        }
+    }
+}
