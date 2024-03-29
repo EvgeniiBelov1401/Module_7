49e6503e4903af35527fb12f68f3e2b3928073df
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-29 11:51:44 +0300

    Добавил Exercise_7_1_6

---
+++ Module_7/Exercise_7_1_6/Obj.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_1_6
+{
+    internal class Obj
+    {
+        private string name;
+        private string owner;
+        private int length;
+        private int count;
+
+        public Obj(string name, string ownerName,int objLength,int count)
+        {
+            this.name = name;
+            owner = ownerName;
+            length = objLength;
+            this.count = count;
+        }
+    }
+}
---
+++ Module_7/Exercise_7_1_6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_7_1_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
