dec7e76f721eb15faa9d8dd9ab650ca1a2341c81
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-03-29 13:44:13 +0300

    Добавил Exercise_7_2_7

---
+++ Module_7/Exercise_7_2_7/Models/C.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_2_7.Models
+{
+    internal class C:A
+    {
+        public override void Display()
+        {
+            Console.WriteLine("C");
+        }
+    }
+}
---
+++ Module_7/Exercise_7_2_7/Models/D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_2_7.Models
+{
+    internal class D:B
+    {
+        public new void Display()
+        {
+            Console.WriteLine("D");
+        }
+    }
+}
