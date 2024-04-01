7673932a1483c1746830eedf0389a76a4441b4d1
EvgeniiBelov1401 <evgeniibelov1401@gmail.com>
2024-04-01 19:42:09 +0300

    Добавил Exercise_7_6_02

---
+++ Module_7/Exercise_7_6_02/Models/ElectricEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7_6_02.Models
+{
+    internal class ElectricEngine
+    {
+    }
+}
---
+++ Module_7/Exercise_7_6_02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercise_7_6_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
